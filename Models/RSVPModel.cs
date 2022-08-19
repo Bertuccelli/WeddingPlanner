709a4c1685af77604ca94f609000a76a8c3f9258
Amanda Bertuccelli <akbertuccelli@gmail.com>
2022-08-18 22:35:01 -0700

    initial commit

---
+++ Models/RSVPModel.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WeddingPlanner.Models;
+
+public class RSVP
+{
+    public int RSVPID { get; set; }
+    public int WeddingId { get; set; }
+    public int UserId { get; set; }
+}
